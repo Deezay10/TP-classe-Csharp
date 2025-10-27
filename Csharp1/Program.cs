78348dd3e0d02ba912153a5ad9511cfb8c35233d
Deezay <nino.caillaud@gmail.com>
2025-10-27 18:05:23 +0100

    Export ALL

--- Csharp1/Program.cs
+++ Csharp1/Program.cs
@@ -15,6 +15,8 @@ Console.WriteLine("Quel est votre nom de votre école ?");
 classe.Ecole = Console.ReadLine();
 Console.WriteLine("Quel est votre niveau ?");
 classe.Niveau = Console.ReadLine();
+
+// On récupère le chemin d'accès du csv
 Console.WriteLine("Route d'accès de votre fichier csv");
 string path_csv = Console.ReadLine();
 
@@ -23,12 +25,13 @@ List<Personne> listeEleves = [];
 
 using (StreamReader sr = new StreamReader(@path_csv))
 {
+     // Pour ignorer l’en-tête
     string line;
-    bool firstLine = true; // Pour ignorer l’en-tête
+    bool firstLine = true;
 
     while ((line = sr.ReadLine()) != null)
     {
-        if (firstLine) // Ignore la première ligne d'en-têtes
+        if (firstLine) // Ignore la première ligne d'en-tête
         {
             firstLine = false;
             continue;
@@ -36,10 +39,10 @@ using (StreamReader sr = new StreamReader(@path_csv))
                 
         string[] values = line.Split(',');
         
-        //On créer l'objet eleve
+        // On créer l'objet eleve
         Personne eleve = new Personne();
         
-        //On associe les valeurs au bon paramètre de notre objet
+        // On associe les valeurs au bon paramètre de notre objet
         eleve.Id = int.Parse(values[0]);
         eleve.Nom = values[1];
         eleve.Prenom = values[2];
@@ -47,43 +50,43 @@ using (StreamReader sr = new StreamReader(@path_csv))
         String addres = values[4];
         eleve.Taille = int.Parse (values[5]);
         
-        //On sépare le string addres en 3 pour avoir la rue, le code postal et la ville
+        // On sépare le string addres en 3 pour avoir la rue, le code postal et la ville
         List<String> listAddress = addres.Split(';').ToList();
         eleve.AdressDetails = new Details(listAddress[0], int.Parse(listAddress[1]), listAddress[2]);
         
-        //On calcule l'age avec la date de naissance
+        // On calcule l'age avec la date de naissance
         
         DateTime.TryParse(birthdate, out DateTime birth);
         DateTime ajd = DateTime.Today;
 
         int age = DateTime.Now.Year - birth.Year;
 
-        // si mois de naissance est après le mois d'aujourdhui
+        // Si mois de naissance est après le mois d'aujourdhui
         if (ajd.Month < birth.Month ||
-            //  ou si dans le même mois mais que le jour est apres aujourd'hui
+            //  OU si dans le même mois mais que le jour est apres aujourd'hui
             (ajd.Month == birth.Month && ajd.Day < birth.Day))
         {
             age = age - 1;
         }
         
-        //On associe age au paramètre age de l'objet eleve
+        // On associe age au paramètre age de l'objet eleve
         eleve.Age = age;
         
-        //On ajoute eleve dans notre liste d'eleves
+        // On ajoute eleve dans notre liste d'eleves
         listeEleves.Add(eleve);
     }
 }
-//On associe la liste d'élèves à notre objet classe
+// On associe la liste d'élèves à notre objet classe
 classe.Eleves = listeEleves;
 
-//On calcule la taille moyenne de la classe en cm
+// On calcule la taille moyenne de la classe en cm
 int taille_moyenne_cm = (int)classe.Eleves.Average(p => p.Taille);
 
 List<Personne> liste_eleves_plus_grand_moyenne= [];
 // Pour chaque élève de la classe
 foreach (Personne eleve in classe.Eleves)
 {
-    //Si l'élève est plus grand que la moyenne ET qu'il habite à Nantes
+    // Si l'élève est plus grand que la moyenne ET qu'il habite à Nantes
     if (eleve.Taille > taille_moyenne_cm && eleve.AdressDetails.City == "Nantes"){
         // Alors on calcule sa taille en m puis on l'ajoute à la liste des élèves qui sont plus grands que la moyenne
         eleve.Taille = eleve.Taille / 100f;
@@ -106,3 +109,4 @@ foreach (Personne eleve in liste_eleves_tries)
     position += 1;
     
 }
+
